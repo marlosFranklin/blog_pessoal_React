fbf6c3957645879324ca53d09f9a0a4c8db9fc5e
marlosFranklin <marlos-gama@hotmail.com>
2026-05-06 14:26:45 -0300

    feat:adiciona roteamento as paginas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/Nav";
 import Home from "./pages/Home";
+import Cadastro from "./pages/cadastro";
+import Login from "./pages/login";
 
 function App() {
   return (
     <>
-      <div>
-        <Home />
-      </div>
+      <BrowserRouter>
+        <NavBar />
+        <div className=" min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
