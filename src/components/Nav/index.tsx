3a4dd1229d687e53e85b7b9538bfd2dde80f6b63
marlosFranklin <marlos-gama@hotmail.com>
2026-05-14 11:36:42 -0300

    feat: criamor a logica para apresentar os compoentes somente apos o usuario estar logado na aplicacao

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, type ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -13,41 +13,42 @@ function Navbar() {
     navigate("/");
   }
 
-  return (
-    <>
+  let component: ReactNode;
+
+  if (usuario.token !== "") {
+    component = (
       <div
         className="w-full flex justify-center py-4
-                           bg-gray-900 text-white"
+            			   bg-gray-900 text-white"
       >
         <div className="container flex justify-between text-lg mx-8">
-          <Link to="/home" className="text-2xl font-bold text-white">
+          <Link to="/home" className="text-2xl font-bold">
             Blog Pessoal
           </Link>
-          {/*  <p> Bem vindo {usuario.senha}</p> */}
 
           <div className="flex gap-4">
-            <Link to="/postagens" className="hover:underline text-white">
+            <Link to="/postagens" className="hover:underline">
               Postagens
             </Link>
-            <Link to="/temas" className=" hover:underline  text-white">
+            <Link to="/temas" className="hover:underline">
               Temas
             </Link>
-            <Link to="/cadastrartema" className="hover:underline  text-white">
+            <Link to="/cadastrartema" className="hover:underline">
               Cadastrar tema
             </Link>
-            Perfil
-            <Link
-              to=""
-              onClick={logout}
-              className="hover:underline  text-white"
-            >
+            <Link to="/perfil" className="hover:underline">
+              Perfil
+            </Link>
+            <Link to="" onClick={logout} className="hover:underline">
               Sair
             </Link>
           </div>
         </div>
       </div>
-    </>
-  );
+    );
+  }
+
+  return <>{component}</>;
 }
 
 export default Navbar;
